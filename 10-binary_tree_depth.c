5e291777f37d933c971922e2d566e18cdf44c135
omar <omar.a.farouk22>
2023-11-02 14:20:18 -0700

    mandatory task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_depth - function that measures the depth of a node in a binary tree
+ *binary_tree_depth - function that measures the depth of node in a binary tree
  *
  *@tree:  pointer to the node to measure the depth
  *Return: depth of tree
